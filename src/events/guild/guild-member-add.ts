32e112e5a3df176b634927d18304d2a4f7e64c07
0xaust1n <yo861015@gmail.com>
2023-09-24 04:31:18 +0800

    chore(events): add join and leave alert

---
+++ src/events/guild/guild-member-add.ts
@@ -0,0 +1,29 @@
+import { ChannelType, Client, EmbedBuilder, GuildMember } from 'discord.js';
+import 'dotenv/config';
+// alert user when receive role
+const channelId: string = process.env.ALERT_JOIN_CHANNEL || null;
+const name = 'guildMemberAdd';
+const once = false;
+const execute = async (joinMember: GuildMember, client: Client<boolean>) => {
+  if (!!channelId) {
+    const channel = client.channels.cache.get(channelId);
+    if (channel.type === ChannelType.GuildText) {
+      const resultEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setAuthor({ name: joinMember.displayName, iconURL: joinMember.displayAvatarURL() })
+        .setThumbnail(joinMember.displayAvatarURL())
+        .addFields({
+          name: '--加入通知--',
+          value: `${joinMember.displayName} **加入了** :)\r
+          請至<#1122160395361194054>報名! \r
+          如有其他問題私訊 <@486147901802938370> 或 <@270118954805100545>`,
+        })
+
+        .setTimestamp();
+
+      channel.send({ embeds: [resultEmbed] });
+    }
+  }
+};
+
+export { execute, name, once };
---
+++ src/events/guild/guild-member-leave.ts
@@ -0,0 +1,27 @@
+import { ChannelType, Client, EmbedBuilder, GuildMember } from 'discord.js';
+import 'dotenv/config';
+// alert user when receive role
+const channelId: string = process.env.ALERT_JOIN_CHANNEL || null;
+const name = 'guildMemberRemove';
+const once = false;
+const execute = async (leaveMember: GuildMember, client: Client<boolean>) => {
+  if (!!channelId) {
+    const channel = client.channels.cache.get(channelId);
+    if (channel.type === ChannelType.GuildText) {
+      const resultEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setAuthor({ name: leaveMember.displayName, iconURL: leaveMember.displayAvatarURL() })
+        .setThumbnail(leaveMember.displayAvatarURL())
+        .addFields({
+          name: '--離開通知--',
+          value: `${leaveMember.displayName} **離開了** :(`,
+        })
+
+        .setTimestamp();
+
+      channel.send({ embeds: [resultEmbed] });
+    }
+  }
+};
+
+export { execute, name, once };
